270b6ce323995a6ae220dbae8eb094fc357b8368
Chris <chris79gp@outlook.com>
2024-01-20 22:19:24 +0000

    chore: move contents of script.js to new module gameBoard.js and remove IIFE example from gameBoard.js

---
+++ gameBoard.js
@@ -0,0 +1,58 @@
+const gameBoard = (function () {
+    const grid = {
+        a: ['', '', ''],
+        b: ['', '', ''],
+        c: ['', '', '']
+    };
+
+    const markCell = function (row, column, marker) {
+        if (grid[row][column] !== '') {
+            return 'Cell is already filled';
+        }
+        grid[row][column] = marker;
+    }
+
+    const clearAllCells = function () {
+        Object.values(grid).forEach(row => row.fill(''));
+    }
+
+    const checkForWinner = function (marker) {
+        return checkRows(marker) || checkColumns(marker) || checkDiagonals(marker);
+    }
+
+    const checkRows = function (marker) {
+        for (const row in grid) {
+            if (grid[row].every(cell => cell === marker)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkColumns = function (marker) {
+        const columns = Object.keys(grid).map(row => Object.values(grid[row]));
+        for (const col in columns) {
+            if (columns[col].every(cell => cell === marker)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkDiagonals = function (marker) {
+        const diagonals = [
+            [grid['a'][0], grid['b'][1], grid['c'][2]],
+            [grid['a'][2], grid['b'][1], grid['c'][0]]
+        ]
+        for (const diagonal of diagonals) {
+            if (diagonal.every(cell => cell === marker)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return { markCell }
+})();
+
+module.exports(gameBoard);
