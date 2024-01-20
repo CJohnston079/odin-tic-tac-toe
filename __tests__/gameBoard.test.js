06b6e7fb183b611068f73f7158c8599165ee2345
Chris <chris79gp@outlook.com>
2024-01-20 22:54:44 +0000

    test: add unit test for gameBoard.markCell

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -1,1 +1,11 @@
 const { gameBoard } = require('../gameBoard');
+
+describe("gameBoard", () => {
+    describe("markCell", () => {
+        it("should return 'Cell is already filled' if the cell is already marked", () => {
+            gameBoard.markCell('b', 1, 'O');
+            const result = gameBoard.markCell('b', 1, 'x');
+            expect(result).toBe('Cell is already filled');
+        });
+    });
+});
