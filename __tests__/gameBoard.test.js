00b109c215edb7e0972bf25ada8cd9588c044a82
Chris <chris79gp@outlook.com>
2024-01-21 16:41:31 +0000

    test: add unit test for gameBoard.resetGrid

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -1,25 +1,34 @@
 const { gameBoard } = require('../gameBoard');
 
-describe("gameBoard", () => {
+describe("resetGrid", () => {
+    beforeEach(() => {
+        gameBoard.markCell('b', 1, 'x');
+        gameBoard.markCell('a', 2, 'o');
+    });
+    it("should set each value in a grid to empty", () => {
+        gameBoard.resetGrid();
+        const result = Object.values(gameBoard.getGrid()).flat();
+        result.forEach(cell => expect(cell).toBe(''));
+    })
+});
+
+describe("markCell", () => {
     beforeEach(() => {
         gameBoard.resetGrid();
     });
-    
-    describe("markCell", () => {
-        it("should returns 'Cell is out of bounds' if row or column is invalid", () => {
-            const invalidRowInput = gameBoard.markCell('d', 1, 'x');
-            const invalidColInput = gameBoard.markCell('a', 4, 'x');
-            expect(invalidRowInput).toBe('Cell is out of bounds');
-            expect(invalidColInput).toBe('Cell is out of bounds');
-        })
-        it("should return 'Cell is already filled' if the cell is already marked", () => {
-            gameBoard.markCell('b', 1, 'O');
-            const result = gameBoard.markCell('b', 1, 'x');
-            expect(result).toBe('Cell is already filled');
-        });
-        it("should mark a cell with the specified marker", () => {
-            gameBoard.markCell('a', 0, 'x');
-            expect(gameBoard.getGrid()['a'][0]).toEqual('x');
-        });
+    it("should return 'Cell is out of bounds' if row or column is invalid", () => {
+        const invalidRowInput = gameBoard.markCell('d', 1, 'x');
+        const invalidColInput = gameBoard.markCell('a', 4, 'x');
+        expect(invalidRowInput).toBe('Cell is out of bounds');
+        expect(invalidColInput).toBe('Cell is out of bounds');
+    })
+    it("should return 'Cell is already filled' if the cell is already marked", () => {
+        gameBoard.markCell('b', 1, 'O');
+        const result = gameBoard.markCell('b', 1, 'x');
+        expect(result).toBe('Cell is already filled');
+    });
+    it("should mark a cell with the specified marker", () => {
+        gameBoard.markCell('a', 0, 'x');
+        expect(gameBoard.getGrid()['a'][0]).toEqual('x');
     });
 });
