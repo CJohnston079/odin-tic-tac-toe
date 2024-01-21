60d3641e502350521663faac780ebf26bda30d0f
Chris <chris79gp@outlook.com>
2024-01-21 11:43:56 +0000

    test: add setupTestGrid function to initialise a mock gameBoard.grid object

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -1,6 +1,18 @@
 const { gameBoard } = require('../gameBoard');
 
+function setupTestGrid() {
+    return {
+        a: ['', '', ''],
+        b: ['', '', ''],
+        c: ['', '', '']
+    };
+}
+
 describe("gameBoard", () => {
+    beforeEach(() => {
+        gameBoard.grid = setupTestGrid();
+    });
+
     describe("markCell", () => {
         it("should return 'Cell is already filled' if the cell is already marked", () => {
             gameBoard.markCell('b', 1, 'O');
