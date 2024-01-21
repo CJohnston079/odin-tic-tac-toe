cb67056705a6317d3c4d2ad84943fbe830879055
Chris <chris79gp@outlook.com>
2024-01-21 12:42:01 +0000

    test: fix test case for testing gameBoard.markCell marks a cell correctly replace setupTestGrid in beforeEach statement with gameBoard.resetGrid replace gameBoard.grid statement with gameBoard.checkGrid method on line 11

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -1,19 +1,16 @@
 const { gameBoard } = require('../gameBoard');
 
-function setupTestGrid() {
-    return {
-        a: ['', '', ''],
-        b: ['', '', ''],
-        c: ['', '', '']
-    };
-}
-
 describe("gameBoard", () => {
     beforeEach(() => {
-        gameBoard.grid = setupTestGrid();
+        gameBoard.resetGrid();
     });
-
+    
     describe("markCell", () => {
+        it("should mark a cell with the specified marker", () => {
+            gameBoard.markCell('a', 0, 'x');
+            expect(gameBoard.getGrid()['a'][0]).toEqual('x');
+        });
+
         it("should return 'Cell is already filled' if the cell is already marked", () => {
             gameBoard.markCell('b', 1, 'O');
             const result = gameBoard.markCell('b', 1, 'x');
