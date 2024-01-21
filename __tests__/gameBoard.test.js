c50aa8c3577a42122960d5d66b15b38ffa849f6d
Chris <chris79gp@outlook.com>
2024-01-21 16:13:28 +0000

    test: add unit test for out of bounds input for gameBoard.markCell

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -6,15 +6,20 @@ describe("gameBoard", () => {
     });
     
     describe("markCell", () => {
-        it("should mark a cell with the specified marker", () => {
-            gameBoard.markCell('a', 0, 'x');
-            expect(gameBoard.getGrid()['a'][0]).toEqual('x');
-        });
-
+        it("should returns 'Cell is out of bounds' if row or column is invalid", () => {
+            const invalidRowInput = gameBoard.markCell('d', 1, 'x');
+            const invalidColInput = gameBoard.markCell('a', 4, 'x');
+            expect(invalidRowInput).toBe('Cell is out of bounds');
+            expect(invalidColInput).toBe('Cell is out of bounds');
+        })
         it("should return 'Cell is already filled' if the cell is already marked", () => {
             gameBoard.markCell('b', 1, 'O');
             const result = gameBoard.markCell('b', 1, 'x');
             expect(result).toBe('Cell is already filled');
+        });
+        it("should mark a cell with the specified marker", () => {
+            gameBoard.markCell('a', 0, 'x');
+            expect(gameBoard.getGrid()['a'][0]).toEqual('x');
         });
     });
 });
