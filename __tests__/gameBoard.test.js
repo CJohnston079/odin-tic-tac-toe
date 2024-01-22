679dd60fb8b7879697c1150dc5798d5fc022a5fb
Chris <chris79gp@outlook.com>
2024-01-22 13:49:20 +0000

    refactor: replace all instances of double quotes with singles for consistency

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -7,36 +7,36 @@ describe('getGrid', () => {
     });
 });
 
-describe("resetGrid", () => {
+describe('resetGrid', () => {
     beforeEach(() => {
         gameBoard.markCell('b', 1, 'x');
         gameBoard.markCell('a', 2, 'o');
     });
-    it("should set each value in a grid to empty", () => {
+    it('should set each value in a grid to empty', () => {
         gameBoard.resetGrid();
         const result = Object.values(gameBoard.getGrid()).flat();
         result.forEach(cell => expect(cell).toBe(''));
     })
 });
 
-describe("markCell", () => {
+describe('markCell', () => {
     beforeEach(() => {
         gameBoard.resetGrid();
     });
-    it("should throw 'Cell is out of bounds' if row or column is invalid", () => {
+    it('should throw "Cell is out of bounds" if row or column is invalid', () => {
         const invalidRowInput = () => gameBoard.markCell('d', 1, 'x');
         const invalidColInput = () => gameBoard.markCell('a', 4, 'x');
         expect(invalidRowInput).toThrow('Cell is out of bounds');
         expect(invalidColInput).toThrow('Cell is out of bounds');
     })
-    it("should throw 'Cell is already filled' if the cell is already marked", () => {
+    it('should throw "Cell is already filled" if the cell is already marked', () => {
         const markFilledCell = () => {
             gameBoard.markCell('b', 1, 'O');
             gameBoard.markCell('b', 1, 'x');
         }
         expect(markFilledCell).toThrow('Cell is already filled');
     });
-    it("should mark a cell with the specified marker", () => {
+    it('should mark a cell with the specified marker', () => {
         gameBoard.markCell('a', 0, 'x');
         gameBoard.markCell('b', 1, 'x');
         gameBoard.markCell('c', 2, 'x');
@@ -46,26 +46,26 @@ describe("markCell", () => {
     });
 });
 
-describe("checkForWinner", () => {
+describe.only('checkForWinner', () => {
     beforeEach(() => {
         gameBoard.resetGrid();
     })
-    it("should return false if no winner is found", () => {
+    it('should return false if no winner is found', () => {
         expect(gameBoard.checkForWinner()).toBe(false);
     });
-    it("should return true if any row has been filled", () => {
+    it('should return true if any row has been filled', () => {
         gameBoard.markCell('a', 0, 'x');
         gameBoard.markCell('a', 1, 'x');
         gameBoard.markCell('a', 2, 'x');
         expect(gameBoard.checkForWinner()).toBe(true);
     });
-    it("should return true if any column has been filled", () => {
+    it('should return true if any column has been filled', () => {
         gameBoard.markCell('a', 0, 'x');
         gameBoard.markCell('b', 0, 'x');
         gameBoard.markCell('c', 0, 'x');
         expect(gameBoard.checkForWinner()).toBe(true);
     });
-    it("should return true if any diagonal has been filled", () => {
+    it('should return true if any diagonal has been filled', () => {
         gameBoard.markCell('a', 0, 'x');
         gameBoard.markCell('b', 1, 'x');
         gameBoard.markCell('c', 2, 'x');
