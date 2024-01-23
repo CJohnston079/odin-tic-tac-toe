d1187c8830ff7b7110e01a74d899fba684033400
Chris <chris79gp@outlook.com>
2024-01-23 15:51:44 +0000

    test: add unit tests for player properties

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,1 +1,14 @@
-const { player } = require('../gameBoard');
+const { player } = require('../player');
+
+describe("player", () => {
+    const testPlayer = player('Milo', 'x');
+
+    test('Player has name and marker properties', () => {
+        expect(testPlayer).toHaveProperty('name');
+        expect(testPlayer).toHaveProperty('marker');
+    });
+    test('Player has correct name and marker values', () => {
+        expect(testPlayer.name).toBe('Milo');
+        expect(testPlayer.marker).toBe('x');
+    });
+})
