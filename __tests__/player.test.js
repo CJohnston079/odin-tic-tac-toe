186edd6aa9fec8a7c2460bf593f2406324c0fecd
Chris <chris79gp@outlook.com>
2024-01-23 20:39:56 +0000

    test: add unit tests for player.isHuman and player.getIsHuman

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,14 +1,33 @@
 const { player } = require('../player');
 
-describe("player", () => {
+describe('player', () => {
     const testPlayer = player('Milo', 'x');
 
-    test('Player has name and marker properties', () => {
+    it('should have name and marker properties', () => {
         expect(testPlayer).toHaveProperty('name');
         expect(testPlayer).toHaveProperty('marker');
     });
-    test('Player has correct name and marker values', () => {
+    it('should have name and marker values', () => {
         expect(testPlayer.name).toBe('Milo');
         expect(testPlayer.marker).toBe('x');
     });
+    it('should have isHuman property which defaults to false', () => {
+        expect(testPlayer.getIsHuman()).toBe(false);
+    });
+    it('should have getIsHuman method', () => {
+        expect(testPlayer).toHaveProperty('getIsHuman');
+        expect(typeof testPlayer.getIsHuman).toBe('function');
+    });
 })
+
+describe('getIsHuman', () => {
+    const testPlayer = player('Milo', 'x');
+
+    it('should have getIsHuman method', () => {
+        expect(testPlayer).toHaveProperty('getIsHuman');
+        expect(typeof testPlayer.getIsHuman).toBe('function');
+    });
+    it('should return player.isHuman', () => {
+        expect(testPlayer.getIsHuman()).toBe(false);
+    })
+});
