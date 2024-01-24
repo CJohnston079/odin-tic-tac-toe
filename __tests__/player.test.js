0159522aa43aca713fb94a7bc117389d2a1bf186
Chris <chris79gp@outlook.com>
2024-01-24 10:22:59 +0000

    test: add unit tests for player.score, player.getScore, player.incrementScore and player.resetScore

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,3 +1,4 @@
+const { experiments } = require('webpack');
 const { player } = require('../player');
 
 const testPlayer = player('Milo', 'x');
@@ -14,15 +15,10 @@ describe('player', () => {
     it('should have isHuman property which defaults to false', () => {
         expect(testPlayer.getIsHuman()).toBe(false);
     });
-    it('should have getIsHuman method', () => {
-        expect(testPlayer).toHaveProperty('getIsHuman');
-        expect(typeof testPlayer.getIsHuman).toBe('function');
-    });
-    it('should have toggleIsHuman method', () => {
-        expect(testPlayer).toHaveProperty('toggleIsHuman');
-        expect(typeof testPlayer.toggleIsHuman).toBe('function');
-    });
-})
+    it('should have a score property initialised to 0', () => {
+        expect(testPlayer.getScore()).toBe(0);
+    })
+});
 
 describe('getIsHuman', () => {
     it('should have getIsHuman method', () => {
@@ -46,3 +42,41 @@ describe('toggleIsHuman', () => {
         expect(testPlayer.getIsHuman()).toBe(false);
     });
 });
+
+describe('getScore', () => {
+    it('should have getScore method', () => {
+        expect(testPlayer).toHaveProperty('getScore');
+        expect(typeof testPlayer.getScore).toBe('function');
+    });
+    it('should return player.score', () => {
+        expect(testPlayer.getScore()).toBe(0);
+    });
+});
+
+describe('incrementScore', () => {
+    afterEach(() => {
+        testPlayer.resetScore()
+    });
+    it('should have incrementScore method', () => {
+        expect(testPlayer).toHaveProperty('incrementScore');
+        expect(typeof testPlayer.incrementScore).toBe('function');
+    });
+    it('should incremend player.score by 1 when invoked', () => {
+        testPlayer.incrementScore();
+        expect(testPlayer.getScore()).toBe(1);
+    });
+});
+
+describe('resetScore', () => {
+    beforeEach(() => {
+        testPlayer.incrementScore()
+    });
+    it('should have resetScore method', () => {
+        expect(testPlayer).toHaveProperty('resetScore');
+        expect(typeof testPlayer.resetScore).toBe('function');
+    });
+    it('should reset player.score to 0 when invoked', () => {
+        testPlayer.resetScore();
+        expect(testPlayer.getScore()).toBe(0);
+    })
+})
