7ac53597dcfb05e310ecf415cd335b1a43fc1ee4
Chris <chris79gp@outlook.com>
2024-01-24 10:23:31 +0000

    feat: add property player.score and methods player.getScore, player.incrementScore and player.resetScore

--- player.js
+++ player.js
@@ -1,10 +1,23 @@
 const player = function (name, marker) {
     let isHuman = false;
+    let score = 0;
 
     const getIsHuman = () => isHuman;
     const toggleIsHuman = () => isHuman = !isHuman;
 
-    return { name, marker, getIsHuman, toggleIsHuman };
+    const getScore = () => score;
+    const incrementScore = () => score += 1;
+    const resetScore = () => score = 0;
+
+    return {
+        name,
+        marker,
+        getIsHuman,
+        toggleIsHuman,
+        getScore,
+        incrementScore,
+        resetScore
+    };
 };
 
 module.exports = { player };
